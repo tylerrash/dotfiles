d282d8ac8518fdf355dc450a4565b2750ca5d835
Tyler Rash <tyler.rash@gmail.com>
2015-07-13 21:57:49 -0400

    Sort plugins

--- init/vim-plugins.sh
+++ init/vim-plugins.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 plugins=(
-   'git@github.com:kien/ctrlp.vim.git'
-   'git@github.com:scrooloose/nerdcommenter.git'
-   'git@github.com:scrooloose/nerdtree.git'
-   'git@github.com:ervandew/supertab.git'
-   'git@github.com:mkitt/tabline.vim.git'
    'git@github.com:SirVer/ultisnips.git'
    'git@github.com:altercation/vim-colors-solarized.git'
    'git@github.com:easymotion/vim-easymotion.git'
-   'git@github.com:tpope/vim-fugitive.git'
-   'git@github.com:pangloss/vim-javascript.git'
+   'git@github.com:ervandew/supertab.git'
    'git@github.com:jistr/vim-nerdtree-tabs.git'
+   'git@github.com:kien/ctrlp.vim.git'
+   'git@github.com:mkitt/tabline.vim.git'
+   'git@github.com:pangloss/vim-javascript.git'
+   'git@github.com:scrooloose/nerdcommenter.git'
+   'git@github.com:scrooloose/nerdtree.git'
+   'git@github.com:tpope/vim-fugitive.git'
    'git@github.com:tpope/vim-surround.git'
 )
 
